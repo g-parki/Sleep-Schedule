991a62d15c7f1aefd930c6a38766b5f4b9706560
parkisongw <parkisongw@gmail.com>
2021-05-10 22:19:18 -0700

    Include response text in success alert

--- static/main.js
+++ static/main.js
@@ -7,8 +7,8 @@ $(function(){
             data: value,
             type: 'POST',
             success: function(response){
-                $(".alert-container").prepend("<div class='alert alert-warning alert-dismissible fade show' role='alert'><strong>Success!</strong> Photo classified.<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>");
-                $(".alert").delay(2000).slideUp(200, function() {
+                $(".alert-container").prepend("<div class='alert alert-success alert-dismissible fade show' role='alert'><strong>Success! </strong>" + response + "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>");
+                $(".alert").delay(3000).slideUp(200, function() {
                     $(this).removeClass('show');
                 });
             },
